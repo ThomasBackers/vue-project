467cdfb0f709cb888323a0b71b78fc6c92e0d0aa
Thomas Backers <thomasbackers@gmail.com>
2022-02-03 23:53:17 +0100

    let's create a v-form to never ever entirely rewrite one ;)

---
+++ src/components/V-Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <form action="" method="post">
+    <img src="" alt="">
+
+    <h3></h3>
+
+    <label for="">
+      label
+      <input type="text">
+    </label>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'V-Form'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
