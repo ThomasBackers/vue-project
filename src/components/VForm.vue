84d5ec4286a01361dbc710f8ec5e0abe503f8b2a
Thomas Backers <thomasbackers@gmail.com>
2022-02-04 01:56:57 +0100

    commit before the night

---
+++ src/components/VForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form action="" method="post">
+    <img src="" alt="">
+
+    <h3 v-if="heading">{{ heading }}</h3>
+
+    <VInput>
+  </form>
+</template>
+
+<script>
+import VInput from '@/components/VInput.vue'
+
+export default {
+  name: 'VForm',
+  components: {
+    VInput
+  },
+  props: ['heading']
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
